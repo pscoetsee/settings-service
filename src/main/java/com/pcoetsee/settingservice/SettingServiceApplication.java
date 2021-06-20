151b195a3ea090844c4114bd78bd6adc7cd30cfe
Phillip <phillip@ratendate.com>
2021-06-20 17:10:34 -0400

    Innitial commit

---
+++ src/main/java/com/pcoetsee/settingservice/SettingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pcoetsee.settingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SettingServiceApplication.class, args);
+	}
+
+}
